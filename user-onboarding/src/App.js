914f0b2875a1b2727a103748383954615e058265
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-08-19 13:36:34 -0500

    Edit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Form.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>User Onboarding Form</h1>
+    <Form />
+     
+    </div>
+  );
+}
+
+export default App;
